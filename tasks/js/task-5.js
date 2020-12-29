898474453fba442dfcd210f858ef6ff650bfcb39
TMFV <68297974+TMFV@users.noreply.github.com>
2020-12-29 19:34:27 +0200

    task-5 add if
    add if = 0

--- tasks/js/task-5.js
+++ tasks/js/task-5.js
@@ -4,5 +4,9 @@ const textDisplayElementRef = document.querySelector('span#name-output');
 textInputRef.addEventListener('input', textHandler);
 
 function textHandler(event) {
-    textDisplayElementRef.textContent = event.target.value; 
+    if (event.target.value.length > 0) {
+        textDisplayElementRef.textContent = event.target.value;
+    } else {
+        textDisplayElementRef.textContent = 'незнакомец';
+    };
 };
